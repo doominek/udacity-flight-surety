517d72d3492dd19886ab889780b899306079e4b8
Dominik Flisak <d.flisak@gmail.com>
2020-02-24 23:32:15 +0100

    feat: contract for registering airlines with approve/reject functionality

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -4,5 +4,5 @@ const FlightSuretyData = artifacts.require("FlightSuretyData");
 module.exports = async (deployer, network, accounts) => {
     await deployer.deploy(FlightSuretyData);
     await FlightSuretyData.deployed();
-    await deployer.deploy(FlightSuretyApp, accounts[1]);
+    await deployer.deploy(FlightSuretyApp, web3.utils.utf8ToHex("Lufthansa"), accounts[1]);
 };
