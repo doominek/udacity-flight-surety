a03bdad0801732d94f837d40fcf66ef316503298
Dominik Flisak <d.flisak@gmail.com>
2020-02-27 00:50:12 +0100

    test: add tests for registering new airlines without consensus

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -5,4 +5,5 @@ module.exports = async (deployer, network, accounts) => {
     await deployer.deploy(FlightSuretyData);
     await FlightSuretyData.deployed();
     await deployer.deploy(FlightSuretyApp, web3.utils.utf8ToHex("Lufthansa"), accounts[1]);
+
 };
