915fbdafea3f38d4efaa0d7a804b54a92dab2e2a
Dominik Flisak <d.flisak@gmail.com>
2020-02-26 23:58:31 +0100

    refactor: migrate pausing tests to Typescript and chai/expect style

---
+++ test/flight-surety.pausing.ts
@@ -0,0 +1,66 @@
+import { FlightSuretyAppInstance } from '../generated/contracts';
+
+import { expect } from 'chai';
+
+const FlightSuretyApp = artifacts.require('FlightSuretyApp');
+
+contract('FlightSuretyApp - pausing (operationl status)', async (accounts) => {
+    let instance: FlightSuretyAppInstance;
+
+    const [
+        owner,
+        firstAirline
+    ] = accounts;
+
+    before(async () => {
+        instance = await FlightSuretyApp.deployed();
+    });
+
+    it('should be unpaused initially', async () => {
+        const state = await instance.paused();
+
+        expect(state, 'Contract should not be paused').to.be.false;
+    });
+
+    describe('after pausing', async () => {
+        before(async () => {
+            await instance.pause();
+        });
+
+        it('should be paused', async () => {
+            const state = await instance.paused();
+
+            expect(state, 'Contract should be paused').to.be.true;
+        });
+
+        it('should not allow calling registerOracle()', async () => {
+            try {
+                await instance.registerOracle();
+                assert.fail('should throw error');
+            } catch (e) {
+                expect(e.reason).to.be.equal('Pausable: paused');
+            }
+        });
+
+        it('should not allow to be unpaused by anyone besides pauser', async () => {
+            try {
+                await instance.unpause({ from: firstAirline });
+                assert.fail('should throw error');
+            } catch (e) {
+                expect(e.reason).to.be.equal('PauserRole: caller does not have the Pauser role')
+            }
+        });
+
+        describe('when unpausing', async () => {
+            before(async () => {
+                await instance.unpause({ from: owner });
+            });
+
+            it('should be unpaused again', async () => {
+                const state = await instance.paused();
+
+                expect(state, 'Contract should not be paused').to.be.false;
+            });
+        });
+    });
+});
