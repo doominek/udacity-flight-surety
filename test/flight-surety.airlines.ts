550ae555ea91051dc89f444dc51894ce751040c5
Dominik Flisak <d.flisak@gmail.com>
2020-02-26 23:48:52 +0100

    test: add tests for checking initialization and airline registration

--- test/flight-surety.airlines.ts
+++ test/flight-surety.airlines.ts
@@ -1,7 +1,7 @@
 import { FlightSuretyAppInstance } from '../generated/contracts';
+import { expect } from 'chai';
 
 const FlightSuretyApp = artifacts.require('FlightSuretyApp');
-import { expect } from 'chai';
 
 contract('FlightSuretyApp - Airlines', async (accounts) => {
     let instance: FlightSuretyAppInstance;
@@ -20,10 +20,41 @@ contract('FlightSuretyApp - Airlines', async (accounts) => {
         instance = await FlightSuretyApp.deployed();
     });
 
+    describe('when contract initialized', () => {
+        it('first airline should be registered automatically', async () => {
+            const isAirline = await instance.isAirline(firstAirline);
+            expect(isAirline).to.be.true;
+        });
+    });
+
+    describe('when registering new airline', () => {
+        it('should be allowed only for other airlines', async () => {
+            try {
+                await instance.registerAirline(
+                    web3.utils.utf8ToHex('Virgin'), firstCandidateAirline,
+                    { from: owner });
+                assert.fail('should throw error');
+            } catch (e) {
+                assert.equal(e.reason, 'AirlineRole: caller does not have the Airline role')
+            }
+        });
+
+        it('should be allowed only when funding fee paid', async () => {
+            try {
+                await instance.registerAirline(
+                    web3.utils.utf8ToHex('Virgin'), firstCandidateAirline,
+                    { from: secondAirline });
+                assert.fail('should throw error');
+            } catch (e) {
+                assert.equal(e.reason, 'Funding fee not paid')
+            }
+        });
+    });
+
     describe('when registering 5th airline', () => {
         it('should be accepted after 2 requests approved', async () => {
             await instance.registerAirline(web3.utils.utf8ToHex('PLL Lot - 1'), firstCandidateAirline, { from: firstAirline });
-            await instance.approveAirlineJoinRequest(firstCandidateAirline, { from: secondAirline });
+            await instance.acceptAirlineJoinRequest(firstCandidateAirline, { from: secondAirline });
 
             const airlines = await instance.getAllAirlines();
 
@@ -31,16 +62,23 @@ contract('FlightSuretyApp - Airlines', async (accounts) => {
         });
     });
 
-    describe('when registering 6th airline', () => {
+    describe.skip('when registering 6th airline', () => {
         it('should be accepted after 3 requests approved', async () => {
             await instance.registerAirline(web3.utils.utf8ToHex('PLL Lot - 2'), secondCandidateAirline, { from: firstAirline });
-            await instance.approveAirlineJoinRequest(secondCandidateAirline, { from: secondAirline });
-            await instance.approveAirlineJoinRequest(secondCandidateAirline, { from: thirdAirline });
+            await instance.acceptAirlineJoinRequest(secondCandidateAirline, { from: secondAirline });
+            await instance.acceptAirlineJoinRequest(secondCandidateAirline, { from: thirdAirline });
 
-            const airlines = await instance.getAllAirlines();
+            let airlines: any = await instance.getAllAirlines();
 
             expect(airlines[ 0 ]).to.have.lengthOf(6);
+
+            await instance.submitFundingFee({ from: secondCandidateAirline, value: web3.utils.toWei('1', 'ether') });
+
+            airlines = await instance.getAllAirlines();
+
+            console.log(airlines);
+            expect(airlines[ 3 ][ 5 ]).to.be.true;
         });
-    })
+    });
 
 });
