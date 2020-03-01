76678c29f54192537a59122754f628a69a517622
Dominik Flisak <d.flisak@gmail.com>
2020-03-01 21:07:04 +0100

    refactor(flights-api): extract FlightStatusOracle class

--- src/flights-api/src/oracles.ts
+++ src/flights-api/src/oracles.ts
@@ -4,8 +4,20 @@ import Web3 from 'web3';
 import { AbiItem } from 'web3-utils';
 import * as _ from 'lodash';
 
-const MAX_NUMBER_OF_ORACLES = 20;
-let oracles = [];
+const NUMBER_OF_ORACLES = 20;
+
+let oracles: FlightStatusOracle[] = [];
+
+class FlightStatusOracle {
+    constructor(private readonly id: number,
+                public readonly account: string,
+                public readonly indexes: number[]) {
+    }
+
+    toString(): string {
+        return `FlightStatusOracle(id=${this.id},account=${this.account},indexes${this.indexes}`;
+    }
+}
 
 const connect = async () => {
     const web3 = new Web3(new Web3.providers.WebsocketProvider('ws://localhost:8545'));
@@ -27,18 +39,16 @@ const connect = async () => {
 const registerOracles = async (contract: FlightSuretyAppContract, accounts: string[]) => {
     const fee = await contract.methods.ORACLE_REGISTRATION_FEE().call();
 
-    for (let idx = 0; idx < MAX_NUMBER_OF_ORACLES; idx++) {
+    for (let idx = 0; idx < NUMBER_OF_ORACLES; idx++) {
         const account = accounts[20 + idx];
 
         await contract.methods.registerOracle().send({ from: account, value: fee, gas: "0x6691b7" });
         const indexes = await contract.methods.getMyIndexes().call({ from: account });
 
-        oracles.push({ idx, account: account, indexes });
+        oracles[idx] = new FlightStatusOracle(idx, account, indexes.map(i => parseInt(i, 10)));
     }
 
     console.log("oracles => ", oracles);
-
-    return contract;
 };
 
 export {
