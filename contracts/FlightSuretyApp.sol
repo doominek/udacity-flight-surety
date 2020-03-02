9ab45932a16a52aa5a321aebbe7b2e087bf6398b
Dominik Flisak <d.flisak@gmail.com>
2020-03-02 22:19:08 +0100

    refactor: prepare internal functions to extract as data interface for Oracles

--- contracts/FlightSuretyApp.sol
+++ contracts/FlightSuretyApp.sol
@@ -35,6 +35,10 @@ contract FlightSuretyApp is Ownable, Pausable, FlightSuretyAirlines {
      *
      */
     function processFlightStatus(address airline, string memory flight, uint256 timestamp, uint8 statusCode) internal {
+        addFlight(airline, flight, timestamp, statusCode);
+    }
+
+    function addFlight(address airline, string memory flight, uint256 timestamp, uint8 statusCode) internal {
         bytes32 key = getFlightKey(airline, flight, timestamp);
 
         Flight storage flightData = flights[key];
@@ -45,7 +49,12 @@ contract FlightSuretyApp is Ownable, Pausable, FlightSuretyAirlines {
     }
 
     function getFlight(address airline, string calldata flight, uint256 timestamp) view external
-        returns (bool isRegistered, uint8 statusCode, uint256 updatedTimestamp) {
+    returns (bool isRegistered, uint8 statusCode, uint256 updatedTimestamp) {
+        return findFlight(airline, flight, timestamp);
+    }
+
+    function findFlight(address airline, string memory flight, uint256 timestamp) view internal
+    returns (bool isRegistered, uint8 statusCode, uint256 updatedTimestamp) {
         bytes32 key = getFlightKey(airline, flight, timestamp);
         Flight storage flightData = flights[key];
 
@@ -58,13 +67,18 @@ contract FlightSuretyApp is Ownable, Pausable, FlightSuretyAirlines {
         uint8 index = getRandomIndex(msg.sender, keccak256(abi.encode(airline, flight, timestamp)));
 
         // Generate a unique key for storing the request
-        bytes32 key = keccak256(abi.encodePacked(index, airline, flight, timestamp));
-        oracleResponses[key] = ResponseInfo({
-            requester : msg.sender,
-            isOpen : true
-        });
+        addOracleResponseInfo(index, airline, flight, timestamp, msg.sender);
 
         emit OracleRequest(index, airline, flight, timestamp);
+    }
+
+    function addOracleResponseInfo(uint8 index, address airline, string memory flight, uint256 timestamp, address requester) internal {
+        bytes32 key = getOracleResponseInfoKey(index, airline, flight, timestamp);
+
+        oracleResponses[key] = ResponseInfo({
+            requester : requester,
+            isOpen : true
+            });
     }
 
 
@@ -119,43 +133,73 @@ contract FlightSuretyApp is Ownable, Pausable, FlightSuretyAirlines {
 
         uint8[3] memory indexes = generateIndexes(msg.sender);
 
-        oracles[msg.sender] = Oracle({
+        registerOracle(msg.sender, indexes);
+    }
+
+    function registerOracle(address oracle, uint8[3] memory indexes) internal {
+        oracles[oracle] = Oracle({
             isRegistered : true,
             indexes : indexes
             });
     }
 
     function getMyIndexes() view external returns (uint8[3] memory) {
-        require(oracles[msg.sender].isRegistered, "Not registered as an oracle");
+        require(isOracleRegistered(msg.sender), "Not registered as an oracle");
 
-        return oracles[msg.sender].indexes;
+        return getOracleIndexes(msg.sender);
+    }
+
+    function getOracleIndexes(address oracle) view internal returns (uint8[3] memory) {
+        return oracles[oracle].indexes;
+    }
+
+    function isOracleRegistered(address oracle) view internal returns (bool) {
+        return oracles[oracle].isRegistered;
+    }
+
+    function isIndexAssignedToOracle(uint8 index, address oracle) view internal returns (bool) {
+        return (oracles[oracle].indexes[0] == index) || (oracles[oracle].indexes[1] == index) || (oracles[oracle].indexes[2] == index);
+    }
+
+    function isOracleResponseInfoOpen(uint8 index, address airline, string memory flight, uint256 timestamp) internal view returns (bool) {
+        bytes32 key = getOracleResponseInfoKey(index, airline, flight, timestamp);
+        return oracleResponses[key].isOpen;
+    }
+
+    function registerOracleResponse(uint8 index, address airline, string memory flight, uint256 timestamp, uint8 statusCode, address oracle) internal {
+        bytes32 key = getOracleResponseInfoKey(index, airline, flight, timestamp);
+        oracleResponses[key].responses[statusCode].push(oracle);
+    }
+
+    function getOracleResponseCountWithStatus(uint8 index, address airline, string memory flight, uint256 timestamp, uint8 statusCode) internal view returns (uint) {
+        bytes32 key = getOracleResponseInfoKey(index, airline, flight, timestamp);
+        return oracleResponses[key].responses[statusCode].length;
+    }
+
+    function closeOracleResponse(uint8 index, address airline, string memory flight, uint256 timestamp) internal {
+        bytes32 key = getOracleResponseInfoKey(index, airline, flight, timestamp);
+        oracleResponses[key].isOpen = false;
     }
 
     // Called by oracle when a response is available to an outstanding request
     // For the response to be accepted, there must be a pending request that is open
     // and matches one of the three Indexes randomly assigned to the oracle at the
     // time of registration (i.e. uninvited oracles are not welcome)
-    function submitOracleResponse(
-        uint8 index,
-        address airline,
-        string calldata flight,
-        uint256 timestamp,
-        uint8 statusCode) external whenNotPaused {
-        require((oracles[msg.sender].indexes[0] == index) || (oracles[msg.sender].indexes[1] == index) || (oracles[msg.sender].indexes[2] == index),
-            "Index does not match oracle request");
+    function submitOracleResponse(uint8 index, address airline, string calldata flight, uint256 timestamp, uint8 statusCode) external whenNotPaused {
+        require(isIndexAssignedToOracle(index, msg.sender), "Index does not match oracle request");
 
-        bytes32 key = keccak256(abi.encodePacked(index, airline, flight, timestamp));
+        bytes32 key = getOracleResponseInfoKey(index, airline, flight, timestamp);
         ResponseInfo storage responseInfo = oracleResponses[key];
 
-        require(responseInfo.isOpen, "Flight or timestamp do not match oracle request");
+        require(isOracleResponseInfoOpen(index, airline, flight, timestamp), "Flight or timestamp do not match oracle request");
 
-        responseInfo.responses[statusCode].push(msg.sender);
+        registerOracleResponse(index, airline, flight, timestamp, statusCode, msg.sender);
         emit OracleReport(airline, flight, timestamp, statusCode);
 
         // Information isn't considered verified until at least MIN_RESPONSES
         // oracles respond with the *** same *** information
-        if (responseInfo.responses[statusCode].length >= MIN_RESPONSES) {
-            responseInfo.isOpen = false;
+        if (getOracleResponseCountWithStatus(index, airline, flight, timestamp, statusCode) >= MIN_RESPONSES) {
+            closeOracleResponse(index, airline, flight, timestamp);
 
             // Handle flight status as appropriate
             processFlightStatus(airline, flight, timestamp, statusCode);
@@ -163,10 +207,11 @@ contract FlightSuretyApp is Ownable, Pausable, FlightSuretyAirlines {
         }
     }
 
-    function getFlightKey(
-        address airline,
-        string memory flight,
-        uint256 timestamp) pure internal returns (bytes32) {
+    function getOracleResponseInfoKey(uint8 index, address airline, string memory flight, uint256 timestamp) pure internal returns (bytes32) {
+        return keccak256(abi.encode(index, airline, flight, timestamp));
+    }
+
+    function getFlightKey(address airline, string memory flight, uint256 timestamp) pure internal returns (bytes32) {
         return keccak256(abi.encode(airline, flight, timestamp));
     }
 
