e7b54c432cd4e8063080902259f3ab501a151652
Dominik Flisak <d.flisak@gmail.com>
2020-03-07 23:33:14 +0100

    fix(dapp): remove todos from store

--- src/dapp/src/store/reducers.ts
+++ src/dapp/src/store/reducers.ts
@@ -1,9 +1,7 @@
 import { combineReducers } from '@reduxjs/toolkit';
-import todos from './todoSlice';
 import blockchain from './blockchainSlice';
 
 const rootReducer = combineReducers({
-    todos,
     blockchain
 });
 
