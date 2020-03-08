70d8d3aff96fb0db5cd385d304280769cd1011d2
Dominik Flisak <d.flisak@gmail.com>
2020-03-08 23:46:56 +0100

    feat(dapp): display table of airlines

--- src/dapp/src/pages/airlines/Airlines.tsx
+++ src/dapp/src/pages/airlines/Airlines.tsx
@@ -1,10 +1,47 @@
-import React from 'react';
-import { useDispatch } from 'react-redux';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { fetchAirlines } from '../../store/airlinesSlice';
+import { Checkbox, Table } from 'semantic-ui-react';
+import { RootState } from '../../store/reducers';
+import { formatAccount } from '../../common/utils';
+import moment from 'moment';
 
 export const Airlines: React.FC = () => {
     const dispatch = useDispatch();
-    dispatch(fetchAirlines());
+    useEffect(() => {
+        dispatch(fetchAirlines());
+    }, []);
 
-    return (<div>Airlines</div>);
+    const { loading, airlines } = useSelector((state: RootState) => ({
+        loading: state.ui.loading,
+        airlines: state.airlines.airlines
+    }));
+
+    if (loading) {
+        return <div>Loading data...</div>;
+    }
+
+    return <Table celled compact definition>
+        <Table.Header fullWidth>
+            <Table.Row>
+                <Table.HeaderCell>Name</Table.HeaderCell>
+                <Table.HeaderCell>Account</Table.HeaderCell>
+                <Table.HeaderCell>Joined</Table.HeaderCell>
+                <Table.HeaderCell>Fee Paid</Table.HeaderCell>
+                <Table.HeaderCell>Actions</Table.HeaderCell>
+            </Table.Row>
+        </Table.Header>
+
+        <Table.Body>
+            {airlines.map((airline, idx) => (
+                <Table.Row key={idx}>
+                    <Table.Cell>{airline.name}</Table.Cell>
+                    <Table.Cell>{formatAccount(airline.account)}</Table.Cell>
+                    <Table.Cell>{moment.unix(airline.date).format('LLL')}</Table.Cell>
+                    <Table.Cell><Checkbox checked={airline.paid}/></Table.Cell>
+                    <Table.Cell></Table.Cell>
+                </Table.Row>
+            ))}
+        </Table.Body>
+    </Table>;
 };
