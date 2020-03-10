eeebf6eb93a9f6e99c098e7f1d17db7de2de98a3
Dominik Flisak <d.flisak@gmail.com>
2020-03-10 12:00:21 +0100

    fix(dapp): add loading screen when connecting to network

--- src/dapp/src/App.tsx
+++ src/dapp/src/App.tsx
@@ -1,20 +1,33 @@
 import React, { useEffect } from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import { Container } from 'semantic-ui-react';
-import { useDispatch } from 'react-redux';
+import { Container, Dimmer, Loader } from 'semantic-ui-react';
+import { useDispatch, useSelector } from 'react-redux';
 import Home from './pages/Home';
 import { initialize } from './store/blockchainSlice';
 import { Airlines } from './pages/airlines/Airlines';
 import { AddAirline } from './pages/airlines/AddAirline';
 import { MainMenu } from './components/MainMenu';
+import { RootState } from './store/reducers';
 
 
 function App() {
     const dispatch = useDispatch();
+
     useEffect(() => {
         dispatch(initialize());
     }, [ dispatch ]);
+
+    const { initialized, error  } = useSelector((state: RootState) => ({
+        initialized: state.blockchain.initialized,
+        error: state.blockchain.error
+    }));
+
+    if (!initialized) {
+        return <Dimmer active inverted>
+            <Loader inverted content='Connecting to network'/>
+        </Dimmer>;
+    }
 
     return <Router>
         <MainMenu/>
