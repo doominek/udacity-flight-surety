bf4f030cbf330781217eb3cb6958787e94cdc8ef
Dominik Flisak <d.flisak@gmail.com>
2020-03-11 20:46:43 +0100

    feat(dapp): add list of finalized requests

--- src/dapp/src/pages/airlines/Requests.tsx
+++ src/dapp/src/pages/airlines/Requests.tsx
@@ -3,9 +3,10 @@ import { useDispatch, useSelector } from 'react-redux';
 import { fetchRequests, voteToAccept, voteToReject } from '../../store/airlinesSlice';
 import { RootState } from '../../store/reducers';
 import { Request, RequestStatus } from '../../types/airlines';
-import { Button, Icon, Label, Table } from 'semantic-ui-react';
+import { Button, Icon, Label, List, Table } from 'semantic-ui-react';
 import { formatAccount } from '../../common/utils';
 import { AsyncAction } from '../../store/uiSlice';
+import { SemanticSIZES } from 'semantic-ui-react/dist/commonjs/generic';
 
 export const Requests = () => {
     const dispatch = useDispatch();
@@ -15,7 +16,7 @@ export const Requests = () => {
     }, [ dispatch ]);
 
 
-    const { action, pendingRequests } = useSelector((state: RootState) => ({
+    const { action, pendingRequests, finalizedRequests } = useSelector((state: RootState) => ({
         action: state.ui.action,
         pendingRequests: state.airlines.requests.filter(r => r.status === RequestStatus.PENDING),
         finalizedRequests: state.airlines.requests.filter(r => r.status !== RequestStatus.PENDING)
@@ -37,17 +38,60 @@ export const Requests = () => {
     }
 
     return <Fragment>
-        <h3>Pending Requests</h3>
         <PendingRequests requests={pendingRequests}
                          acceptingInProgress={request => !!action && isAcceptingInProgress(action, request)}
                          rejectingInProgress={request => !!action && isRejectingInProgress(action, request)}
                          onVoteToAccept={request => dispatch(voteToAccept(request))}
                          onVoteToReject={request => dispatch(voteToReject(request))}/>
-
+        <FinalizedRequests requests={finalizedRequests}/>
     </Fragment>;
 
 };
 
+
+const FinalizedRequests: React.FC<{
+    requests: Request[]
+}> = ({ requests }) => {
+    const renderItem = (request: Request) => {
+        const icon = request.status === RequestStatus.ACCEPTED ? 'check' : 'x';
+        const color = request.status === RequestStatus.ACCEPTED ? 'green' : 'red';
+
+        return <List.Item key={request.account}>
+            <List.Icon color={color} name={icon} size='large' verticalAlign='middle'/>
+            <List.Content>
+                <List.Header as='h4'><Label color='blue'>{request.name}</Label></List.Header>
+                <List.Description as='a'>
+                    <p>{formatAccount(request.account)}</p>
+                    <VotesSummary size='mini' accepted={request.votesAccepted} rejected={request.votesRejected}/>
+                </List.Description>
+            </List.Content>
+        </List.Item>;
+    };
+
+    return <Fragment>
+        <h3>Finalized Requests</h3>
+        <List relaxed divided>
+            {requests.map(request => renderItem(request))}
+        </List>
+    </Fragment>
+};
+
+const VotesSummary: React.FC<{ accepted: number, rejected: number, size?: SemanticSIZES }> = ({
+                                                                                                  accepted,
+                                                                                                  rejected,
+                                                                                                  size = 'small'
+                                                                                              }) => {
+    return <Fragment>
+        <Label size={size}>
+            <Icon name='thumbs up' color='green'/>
+            {accepted}
+        </Label>/
+        <Label size={size}>
+            <Icon name='thumbs down' color='red'/>
+            {rejected}
+        </Label>
+    </Fragment>
+};
 
 const PendingRequests: React.FC<{
     requests: Request[],
@@ -71,34 +115,29 @@ const PendingRequests: React.FC<{
         );
     };
 
-    return <Table celled compact definition>
-        <Table.Header fullWidth>
-            <Table.Row>
-                <Table.HeaderCell>Name</Table.HeaderCell>
-                <Table.HeaderCell>Account</Table.HeaderCell>
-                <Table.HeaderCell>Votes</Table.HeaderCell>
-                <Table.HeaderCell>Actions</Table.HeaderCell>
-            </Table.Row>
-        </Table.Header>
-
-        <Table.Body>
-            {requests.map((request, idx) => (
-                <Table.Row key={idx}>
-                    <Table.Cell>{request.name}</Table.Cell>
-                    <Table.Cell>{formatAccount(request.account)}</Table.Cell>
-                    <Table.Cell>
-                        <Label>
-                            <Icon name='thumbs up' color='green'/>
-                            {request.votesAccepted}
-                        </Label>/
-                        <Label>
-                            <Icon name='thumbs down' color='red'/>
-                            {request.votesRejected}
-                        </Label>
-                    </Table.Cell>
-                    <Table.Cell>{renderVotingButtons(request)}</Table.Cell>
+    return <Fragment>
+        <h3>Pending Requests</h3>
+        <Table celled compact definition>
+            <Table.Header fullWidth>
+                <Table.Row>
+                    <Table.HeaderCell>Name</Table.HeaderCell>
+                    <Table.HeaderCell>Account</Table.HeaderCell>
+                    <Table.HeaderCell>Votes</Table.HeaderCell>
+                    <Table.HeaderCell>Actions</Table.HeaderCell>
                 </Table.Row>
-            ))}
-        </Table.Body>
-    </Table>;
+            </Table.Header>
+
+            <Table.Body>
+                {requests.map((request, idx) => (
+                    <Table.Row key={idx}>
+                        <Table.Cell>{request.name}</Table.Cell>
+                        <Table.Cell>{formatAccount(request.account)}</Table.Cell>
+                        <Table.Cell>
+                            <VotesSummary accepted={request.votesAccepted} rejected={request.votesRejected}/>
+                        </Table.Cell>
+                        <Table.Cell>{renderVotingButtons(request)}</Table.Cell>
+                    </Table.Row>
+                ))}
+            </Table.Body>
+        </Table></Fragment>;
 };
