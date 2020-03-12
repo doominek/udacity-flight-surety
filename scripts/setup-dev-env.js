a0fb0aa33f6262442c47eb6c3db1e24dccf620f1
Dominik Flisak <d.flisak@gmail.com>
2020-03-12 22:28:21 +0100

    chore: add script for filling dev environment with some data

--- scripts/setup-dev-env.js
+++ scripts/setup-dev-env.js
@@ -4,9 +4,20 @@ module.exports = async function(callback) {
     try { 
         const contract = await FlightSuretyApp.deployed();
         const accounts = await web3.eth.getAccounts();
-    
-        await contract.submitFundingFee({ from: accounts[1], value: web3.utils.toWei("1", "ether") });
-    
+
+        const fundingFee = await contract.AIRLINE_FUNDING_FEE({ from: accounts[1] });
+
+        await contract.submitFundingFee({ from: accounts[1], value: fundingFee });
+
+        await contract.registerAirline(web3.utils.utf8ToHex('WhizzAir'), accounts[2], { from: accounts[1] });
+        await contract.submitFundingFee({ from: accounts[2], value: fundingFee });
+
+        await contract.registerAirline(web3.utils.utf8ToHex('Fly Emirates'), accounts[3], { from: accounts[1] });
+        await contract.submitFundingFee({ from: accounts[3], value: fundingFee });
+
+        await contract.registerAirline(web3.utils.utf8ToHex('PLL Lot'), accounts[4], { from: accounts[1] });
+        await contract.registerAirline(web3.utils.utf8ToHex('Air Canada'), accounts[5], { from: accounts[1] });
+
         callback();
     } catch (e) {
         callback(e);
